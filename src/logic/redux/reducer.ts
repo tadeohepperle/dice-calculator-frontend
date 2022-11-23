95d23846b65aad8a303558d1e62a3d9b21233dd7
Tadeo Hepperle <tadeo@do-mix.de>
2022-11-23 16:43:17 +0800

    restructure the root reducer

--- src/logic/redux/reducer.ts
+++ src/logic/redux/reducer.ts
@@ -4,96 +4,36 @@ import type { DiceIndex } from "../data_types";
 import type { Actions } from "./actions";
 import { AppState, DiceInputSegmentState, initialState } from "./state";
 
+let ___: Actions.AppStateAction;
+const actionTypeFunctionMap: Record<
+  typeof ___.type,
+  (state: AppState, action: Actions.AppStateAction) => AppState
+> = {
+  ChangeInput: (s, a) =>
+    changeInputReducer(s, a.payload as Actions.ChangeInputPayload),
+  DeleteDice: (s, a) =>
+    deleteDiceReducer(s, a.payload as Actions.DeleteDicePayload),
+  AddDice: (s, a) => addDiceReducer(s, a.payload as Actions.AddDicePayload),
+  ChangeRollManyNumber: (s, a) =>
+    changeRollManyNumberReducer(
+      s,
+      a.payload as Actions.ChangeRollManyNumberPayload
+    ),
+  CalculateDistribution: (s, a) =>
+    calculateDistributionReducer(
+      s,
+      a.payload as Actions.CalculateDistributionPayload
+    ),
+  Roll: (s, a) => rollReducer(s, a.payload as Actions.RollPayload),
+};
+
 const rootReducer: Reducer<AppState, Actions.AppStateAction> = (
   state: AppState | undefined,
   action: Actions.AppStateAction
-) => {
-  if (!state) {
-    return initialState;
-  }
-  try {
-    if (action.type == "ChangeInput") {
-      let { payload } = action as Actions.ChangeInput;
-
-      let seg: DiceInputSegmentState = {
-        ...state.inputSegments[payload.diceIndex],
-        inputValue: payload.value,
-        calculationState: "newinput",
-      };
-      let segs = state.inputSegments.map((e, i) =>
-        i == payload.diceIndex ? seg : e
-      );
-      return { ...state, inputSegments: segs };
-    } else if (action.type == "DeleteDice") {
-      let { payload } = action as Actions.DeleteDice;
-      let segs: DiceInputSegmentState[] = state.inputSegments
-        .filter((e) => e.diceIndex != payload.diceIndex)
-        .map((e) =>
-          e.diceIndex <= payload.diceIndex
-            ? e
-            : { ...e, diceIndex: (e.diceIndex - 1) as DiceIndex }
-        );
-
-      return { ...state, inputSegments: segs };
-    } else if (action.type == "AddDice") {
-      if (state.inputSegments.length < 3) {
-        let newSeg: DiceInputSegmentState = {
-          diceIndex: state.inputSegments.length as DiceIndex,
-          inputValue: "",
-          calculationState: "newinput",
-          rollManyNumber: 100,
-        };
-        return { ...state, inputSegments: [...state.inputSegments, newSeg] };
-      }
-    } else if (action.type == "CalculateDistribution") {
-      let { payload } = action as Actions.CalculateDistribution;
-
-      let diceString = state.inputSegments[payload.diceIndex].inputValue;
-      const doCalculationsAndUpdateState = async (
-        diceIndex: DiceIndex,
-        diceString: string
-      ) => {
-        if (diceString !== "") {
-          try {
-            let d = await wasmComputeDice(diceString);
-            console.log(`d.build_time: ${d}`);
-          } catch (ex) {
-            console.log(ex);
-          }
-        } else {
-          // remove distribution; TODO
-        }
-      };
-      doCalculationsAndUpdateState(payload.diceIndex, diceString);
-
-      let seg: DiceInputSegmentState = {
-        ...state.inputSegments[payload.diceIndex],
-        calculationState: "calculating",
-      };
-      let segs = state.inputSegments.map((e, i) =>
-        i == payload.diceIndex ? seg : e
-      );
-      return { ...state, inputSegments: segs };
-    } else if (action.type == "Roll") {
-      return { ...state };
-    } else if (action.type == "ChangeRollManyNumber") {
-      let { payload } = action as Actions.ChangeRollManyNumber;
-      let seg: DiceInputSegmentState = {
-        ...state.inputSegments[payload.diceIndex],
-        rollManyNumber: payload.value,
-      };
-      let segs = state.inputSegments.map((e, i) =>
-        i == payload.diceIndex ? seg : e
-      );
-      return { ...state, inputSegments: segs };
-    } else {
-      const _exhaustiveCheck: never = action;
-    }
-  } catch (err) {
-    console.error(err);
-  }
-
-  return state;
+): AppState => {
+  return !state
+    ? initialState
+    : actionTypeFunctionMap[action.type](state, action);
 };
 
 export const store = configureStore({
@@ -103,3 +43,100 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+function changeInputReducer(
+  state: AppState,
+  payload: Actions.ChangeInputPayload
+): AppState {
+  let seg: DiceInputSegmentState = {
+    ...state.inputSegments[payload.diceIndex],
+    inputValue: payload.value,
+    calculationState: "newinput",
+  };
+  let segs = state.inputSegments.map((e, i) =>
+    i == payload.diceIndex ? seg : e
+  );
+  return { ...state, inputSegments: segs };
+}
+
+function deleteDiceReducer(
+  state: AppState,
+  payload: Actions.DeleteDicePayload
+): AppState {
+  let segs: DiceInputSegmentState[] = state.inputSegments
+    .filter((e) => e.diceIndex != payload.diceIndex)
+    .map((e) =>
+      e.diceIndex <= payload.diceIndex
+        ? e
+        : { ...e, diceIndex: (e.diceIndex - 1) as DiceIndex }
+    );
+
+  return { ...state, inputSegments: segs };
+}
+
+function addDiceReducer(
+  state: AppState,
+  payload: Actions.AddDicePayload
+): AppState {
+  if (state.inputSegments.length < 3) {
+    let newSeg: DiceInputSegmentState = {
+      diceIndex: state.inputSegments.length as DiceIndex,
+      inputValue: "",
+      calculationState: "newinput",
+      rollManyNumber: 100,
+    };
+    return { ...state, inputSegments: [...state.inputSegments, newSeg] };
+  } else {
+    return state;
+  }
+}
+
+function changeRollManyNumberReducer(
+  state: AppState,
+  payload: Actions.ChangeRollManyNumberPayload
+): AppState {
+  let seg: DiceInputSegmentState = {
+    ...state.inputSegments[payload.diceIndex],
+    rollManyNumber: payload.value,
+  };
+  let segs = state.inputSegments.map((e, i) =>
+    i == payload.diceIndex ? seg : e
+  );
+  return { ...state, inputSegments: segs };
+}
+
+function calculateDistributionReducer(
+  state: AppState,
+  payload: Actions.CalculateDistributionPayload
+): AppState {
+  let diceString = state.inputSegments[payload.diceIndex].inputValue;
+  const doCalculationsAndUpdateState = async (
+    diceIndex: DiceIndex,
+    diceString: string
+  ) => {
+    if (diceString !== "") {
+      try {
+        let d = await wasmComputeDice(diceString);
+        console.log(`d.build_time: ${d}`);
+      } catch (ex) {
+        console.log(ex);
+      }
+    } else {
+      // remove distribution; TODO
+    }
+  };
+  doCalculationsAndUpdateState(payload.diceIndex, diceString);
+
+  let seg: DiceInputSegmentState = {
+    ...state.inputSegments[payload.diceIndex],
+    calculationState: "calculating",
+  };
+  let segs = state.inputSegments.map((e, i) =>
+    i == payload.diceIndex ? seg : e
+  );
+  return { ...state, inputSegments: segs };
+}
+
+function rollReducer(state: AppState, payload: Actions.RollPayload): AppState {
+  return state;
+}
